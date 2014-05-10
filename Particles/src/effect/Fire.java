989d0eee891fcf7bb71dba1831b1286a3113e248
_Valkryst <symphonic8@mail.com>
2014-05-10 01:43:33 -0300

    Updated documentation of the update() method and changed the color variable names to better reflect what they represent.

--- Particles/src/effect/Fire.java
+++ Particles/src/effect/Fire.java
@@ -11,13 +11,13 @@ import particle.Particle;
  */
 public class Fire extends Effect {
     /** One of the four colors used by the fire particles. */
-	private static final Color ONE = new Color(159, 70, 24, 100);
+	private static final Color REDISH_BROWN = new Color(159, 70, 24, 100);
     /** One of the four colors used by the fire particles. */
-	private static final Color TWO = new Color(208, 117, 29, 100);
+	private static final Color DARK_ORANGE = new Color(208, 117, 29, 100);
     /** One of the four colors used by the fire particles. */
-	private static final Color THREE = new Color(246, 206, 72, 100);
+	private static final Color GOLD = new Color(246, 206, 72, 100);
     /** One of the four colors used by the fire particles. */
-	private static final Color FOUR = new Color(251, 239, 169, 100);
+	private static final Color YELLOW = new Color(251, 239, 169, 100);
 	
 	/**
 	 * Constructs a new Fire object.
@@ -27,16 +27,18 @@ public class Fire extends Effect {
 	}
 	
 	/**
-	 * Updates the fire.
+     * Updates the fire particles by following two simple steps:
+     *     If twenty update calls have been made, then create more particles.
+     *     Call the update methods of all fire particles.
 	 */
 	public void update() {
         // After 20 update calls, create new particles.
         // This is an arbitrary number.
 		if(counter == 20) {
-			for(int i=0;i<10;i++) { newParticle(ONE, 4, 150, 19); }
-			for(int i=0;i<7;i++) { newParticle(TWO, 4, 125, 16); }
-			for(int i=0;i<5;i++) { newParticle(THREE, 4, 100, 13); }
-			for(int i=0;i<2;i++) { newParticle(FOUR, 4, 50, 10); }
+			for(int i=0;i<10;i++) { newParticle(REDISH_BROWN, 4, 150, 19); }
+			for(int i=0;i<7;i++) { newParticle(DARK_ORANGE, 4, 125, 16); }
+			for(int i=0;i<5;i++) { newParticle(GOLD, 4, 100, 13); }
+			for(int i=0;i<2;i++) { newParticle(YELLOW, 4, 50, 10); }
 			counter = 0;
 		} else {
 			counter++;
