9291ad7e8949e2c6e346ce84e7acd0a4a0fa8aca
_Valkryst <symphonic8@mail.com>
2014-08-10 18:52:32 -0300

    Increased the COLOR_CHANGE_CONSTANT value from 0.125 to 0.250. The colors now change much faster.

--- Particles/src/effect/RainbowSnow.java
+++ Particles/src/effect/RainbowSnow.java
@@ -1,8 +1,8 @@
 package effect;
 
-import java.awt.Color;
-
 import particle.Particle;
+
+import java.awt.*;
 
 /**
  * Represents rainbow snow fall.
@@ -13,7 +13,7 @@ public class RainbowSnow extends Effect {
 	/** The length (x-axis) of the screen. */
 	private final double SCREEN_LENGTH;
     /** An arbitrary number which controls how fast the rgb values are changed. */
-	private final double COLOR_CHANGE_CONSTANT = 0.125;
+	private final double COLOR_CHANGE_CONSTANT = 0.250;
     /** An rgb value representing the color to apply to all new particles. */
 	private double red = 255, green = 0, blue = 0;
     /** A boolean value representing which color will be used next. */
