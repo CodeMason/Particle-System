45ca822abc4495d6055f20e1e1de9693b31ce7ed
_Valkryst <symphonic8@mail.com>
2014-08-10 18:44:32 -0300

    Moved all of the JFrame and most of the Screen creation/setup code from the Screen class into the Driver class.
    This makes the code a bit easier to read and figure out.

--- Particles/src/core/Driver.java
+++ Particles/src/core/Driver.java
@@ -1,7 +1,28 @@
 package core;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Driver {
 	public static void main(String[] args) {
-		new Screen();
+        // Create the frame.
+        JFrame frame = new JFrame();
+        frame.setTitle("Particle Test");
+        frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        frame.setExtendedState(Frame.MAXIMIZED_BOTH);
+        frame.setUndecorated(true);
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.requestFocus();
+
+        // Create the screen.
+        Screen screen = new Screen();
+        screen.setSize(new Dimension(frame.getWidth(), frame.getHeight()));
+        screen.setFocusable(true);
+        screen.setVisible(true);
+        screen.setBackground(Color.black);
+        frame.add(screen);
+
+        frame.setVisible(true);
 	}
 }
