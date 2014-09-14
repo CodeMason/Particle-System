3bda54499608eb8847af9bee26e6744dd7e28ae6
_Valkryst <symphonic8@mail.com>
2014-09-14 15:40:55 -0300

    Update final variable names to conform with the standard.

--- Particles/src/particle/Particle.java
+++ Particles/src/particle/Particle.java
@@ -22,41 +22,41 @@ public class Particle {
 	/** The change in Y, per update, of the particle. */
 	private double dy;
 	/** The gravitational pull to the left (negative) and right (positive) acting on this particle. */
-	private final double GRAVITY_X;
+	private final double gravityX;
 	/** The gravitational pull to the up (negative) and down (positive) acting on this particle. */
-	private final double GRAVITY_Y;
+	private final double gravityY;
 	/** The size in pixels^2 of the particle. */
 	private final int SIZE;
 	/** The remaining lifetime of the particle. */
 	private double currentLife;
 	/** The total lifetime of the particle. */
-	private final double TOTAL_LIFE;
+	private final double totalLife;
 	/** The color of the particle. */
 	private Color color;
 	
 	/**
 	 * Constructs a new particle with the specified data.
-	 * @param X The location of the particle on the X-axis.
-	 * @param Y The location of the partivcle on the Y-axis.
-	 * @param DX The change in X, per update, of the particle.
-	 * @param DY The change in Y, per update, of the particle.
-	 * @param GRAVITY_X The gravitational pull to the left (negative) and right (positive) acting on this particle.
-	 * @param GRAVITY_Y The gravitational pull to the up (negative) and down (positive) acting on this particle.
-	 * @param SIZE The size in pixels^2 of the particle.
-	 * @param LIFE The remaining lifetime of the particle.
-	 * @param COLOR The color of the particle.
+	 * @param x The location of the particle on the X-axis.
+	 * @param y The location of the partivcle on the Y-axis.
+	 * @param dx The change in X, per update, of the particle.
+	 * @param dy The change in Y, per update, of the particle.
+	 * @param gravityX The gravitational pull to the left (negative) and right (positive) acting on this particle.
+	 * @param gravityY The gravitational pull to the up (negative) and down (positive) acting on this particle.
+	 * @param size The size in pixels^2 of the particle.
+	 * @param life The remaining lifetime of the particle.
+	 * @param color The color of the particle.
 	 */
-	public Particle(final double X, final double Y, final double DX, final double DY, final double GRAVITY_X, final double GRAVITY_Y, final int SIZE, final double LIFE, final Color COLOR) {
-		x = X;
-		y = Y;
-		dx = DX;
-		dy = DY;
-		this.GRAVITY_X = GRAVITY_X;
-		this.GRAVITY_Y = GRAVITY_Y;
-		this.SIZE = SIZE;
-		currentLife = LIFE;
-		TOTAL_LIFE = LIFE;
-		color = COLOR;
+	public Particle(final double x, final double y, final double dx, final double dy, final double gravityX, final double gravityY, final int size, final double life, final Color color) {
+		this.x = x;
+		this.y = y;
+		this.dx = dx;
+		this.dy = dy;
+		this.gravityX = gravityX;
+		this.gravityY = gravityY;
+		this.SIZE = size;
+		currentLife = life;
+		totalLife = life;
+		this.color = color;
 	}
 	
 	/**
@@ -73,10 +73,10 @@ public class Particle {
 		} else {
 			x += dx;
 			y += dy;
-			dx += GRAVITY_X;
-			dy += GRAVITY_Y;
+			dx += gravityX;
+			dy += gravityY;
 			currentLife--;
-            int alpha = (int)((currentLife/TOTAL_LIFE)*100);
+            int alpha = (int)((currentLife/ totalLife)*100);
 			color = new Color(color.getRed(), color.getGreen(), color.getBlue(), (alpha >= 0 ? alpha : 0));
 			return currentLife <= 0;
 		}
@@ -84,16 +84,16 @@ public class Particle {
 	
 	/**
 	 * Renders the particle to the screen.
-	 * @param G The graphics object to render with.
-	 * @param IS_OVAL Whether or not to render the particle as an oval.
+	 * @param g The graphics object to render with.
+	 * @param isOval Whether or not to render the particle as an oval.
 	 */
-	public void render(final Graphics G, final boolean IS_OVAL) {
-		G.setColor(color);
+	public void render(final Graphics g, final boolean isOval) {
+		g.setColor(color);
 
-		if(IS_OVAL) {
-			G.fillOval((int)x-(SIZE / 2), (int)y-(SIZE / 2), SIZE, SIZE); //x-(size/2) & y-(size/2) make sure the particle is rendered at (x, y).
+		if(isOval) {
+			g.fillOval((int) x - (SIZE / 2), (int) y - (SIZE / 2), SIZE, SIZE); //x-(size/2) & y-(size/2) make sure the particle is rendered at (x, y).
 		} else {
-			G.fillRect((int)x-(SIZE / 2), (int)y-(SIZE / 2), SIZE, SIZE); //x-(size/2) & y-(size/2) make sure the particle is rendered at (x, y).
+			g.fillRect((int) x - (SIZE / 2), (int) y - (SIZE / 2), SIZE, SIZE); //x-(size/2) & y-(size/2) make sure the particle is rendered at (x, y).
 		}
 	}
 }
