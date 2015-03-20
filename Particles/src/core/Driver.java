7884d6e0edbae2cea5a4525cf28db5ee2ca1e7c1
Valkryst <symphonic8@mail.com>
2015-03-20 01:47:00 -0300

    Removed unused code.

--- Particles/src/core/Driver.java
+++ Particles/src/core/Driver.java
@@ -5,39 +5,6 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 public class Driver {
 	public static void main(String[] args) {
-        /*
-        // Create the frame.
-        Frame frame = new JFrame();
-        frame.setTitle("Particle Test");
-        frame.setExtendedState(Frame.MAXIMIZED_BOTH);
-        frame.setUndecorated(true);
-        frame.setResizable(false);
-        frame.setLocationRelativeTo(null);
-        frame.requestFocus();
-        */
-
-        /*
-        // Create the frame.
-        Frame frame = new JFrame();
-        frame.setTitle("Particle Test");
-        frame.setSize(512, 512);
-        frame.setLocationRelativeTo(null);
-        frame.requestFocus();
-
-        // Create the screen.
-        Screen screen = new Screen();
-        screen.setSize(new Dimension(frame.getWidth(), frame.getHeight()));
-        screen.setFocusable(true);
-        screen.setVisible(true);
-        screen.setBackground(Color.black);
-        frame.add(screen);
-        frame.setVisible(true);
-
-        screen.setEffect(frame.getSize());
-
-        screen.start();
-        */
-
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
         config.title = "Particle Test";
         config.width = 512;
