580bfb309d74b077feeac14dd03ac65202e2c6ce
Valkryst <symphonic8@mail.com>
2015-03-21 13:41:01 -0300

    Added in a screenDimension variable to keep track of the screen dimensions for use throughout the class.

--- Particles/src/core/Screen.java
+++ Particles/src/core/Screen.java
@@ -2,11 +2,11 @@ package core;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL30;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import effect.Effect;
-import effect.Fire;
+import effect.*;
 
 import java.awt.*;
 
@@ -20,6 +20,8 @@ public class Screen implements ApplicationListener {
     private ShapeRenderer renderer;
     private Effect effect;
 
+    private Dimension screenDimensions;
+
     @Override
     public void create() {
         camera = new OrthographicCamera();
@@ -27,7 +29,8 @@ public class Screen implements ApplicationListener {
 
         renderer = new ShapeRenderer();
 
-        effect = new Fire(new Dimension(912, 512), 256f, 256f);
+        screenDimensions = new Dimension(512, 512);
+        effect = new Fire(screenDimensions, 256, 256);
     }
 
     @Override
@@ -48,11 +51,26 @@ public class Screen implements ApplicationListener {
     @Override
     public void resize(int width, int height) {
         camera.setToOrtho(false, width, height);
-        effect.setScreenDimensions(new Dimension(width, height), width/2, height/2);
+        screenDimensions = new Dimension(width, height);
+        effect.setScreenDimensions(screenDimensions, width/2, height/2);
     }
 
     @Override
     public void render() {
+        // Quick Effect Swithing
+        if(Gdx.input.isKeyPressed(Input.Keys.F1)) {
+            effect = new Fire(screenDimensions, screenDimensions.width/2, screenDimensions.height/2);
+        } else if(Gdx.input.isKeyPressed(Input.Keys.F2)) {
+            effect = new ParticleHoleEffect(screenDimensions, 0, screenDimensions.height);
+        } else if(Gdx.input.isKeyPressed(Input.Keys.F3)) {
+            effect = new RainbowSnow(screenDimensions, 0, screenDimensions.height);
+        } else if(Gdx.input.isKeyPressed(Input.Keys.F4)) {
+            effect = new Snow(screenDimensions, 0, screenDimensions.height);
+        } else if(Gdx.input.isKeyPressed(Input.Keys.F5)) {
+            effect = new SplitWave(screenDimensions, 0, screenDimensions.height);
+        }
+
+        // Rendering
         Gdx.graphics.getGL30().glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         Gdx.graphics.getGL30().glClear(GL30.GL_COLOR_BUFFER_BIT);
 
